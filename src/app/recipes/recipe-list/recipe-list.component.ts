662e99a491d94a9543a4047c9be6b653c5df6969
ml-astro <ml-astro@users.noreply.github.com>
2022-09-04 13:33:14 +0300

    recipe-list component update

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,23 @@
 import { Component } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component ({
     selector: 'app-recipe-list',
-    templateUrl: 'recipe-list.component.html'
+    templateUrl: 'recipe-list.component.html',
+    styleUrls: ['recipe-list.component.css'],
 })
 
 export class RecipeList {
+    recipes: Recipe[] = [
+        new Recipe('A test recipe', 'Just a test', 'https://picsum.photos/200'),
+        new Recipe('Second recipe', 'Just another test', 'https://picsum.photos/200'),
+        new Recipe('Third recipe', 'It works!', 'https://picsum.photos/200'),
+        new Recipe('A test recipe', 'Just a test', 'https://picsum.photos/200'),
+        new Recipe('Second recipe', 'Just another test', 'https://picsum.photos/200'),
+        new Recipe('Third recipe', 'It works!', 'https://picsum.photos/200')
+    ];
 
+    constructor (){
+
+    }
 }
