62d99a66714bd4063e34f59dfe190c4c15aed879
ml-astro <ml-astro@users.noreply.github.com>
2022-09-04 11:32:16 +0300

    project structure created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeDetail } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItem } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeList } from './recipes/recipe-list/recipe-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingList } from './shopping-list/shopping-list.component';
+import { ShoppingEdit } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeList,
+    RecipeDetail,
+    RecipeItem,
+    ShoppingList,
+    ShoppingEdit
   ],
   imports: [
     BrowserModule
---
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+})
+export class ShoppingEdit {
+
+  constructor() { }
+
+}
